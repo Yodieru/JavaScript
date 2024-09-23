ea6f913e5a713d5c762a75a52e57c8bbbcc3dba7
Yodieru <163355601+Yodieru@users.noreply.github.com>
2024-09-23 13:00:07 -0600

    Aprendimos Objetos y Funciones como tipos de datos

--- 01-Fundamentos/tipos-de-datos.js
+++ 01-Fundamentos/tipos-de-datos.js
@@ -22,11 +22,19 @@
 
   Es el valor que se  le asigna una variable que ha sido declarada pero no inicializada.
 
-  2. Tipos de Datos complejos
+  2. Tipos de Datos complejos o de objetos
 
   *Array o Arreglo
 
   Es un conjunto de Datos que los almacena en una lista ordenada de elementos. Los datos dentro de los arreglos se escriben entre corchetes [] y separardos por comas .
+
+  * Object
+
+  Es un conjunto de multiples datos en pares 'clave'-'valor'. Se escriben entre llaves {} y separados por comas.
+
+  * Fuctions o Funciones
+
+  Esun tipo de dato que puede ser "invocado". Se declaran con una palabra reservada "funcion" y el codigo que se ejecutan entre llave {"codigo"}
 */
 
 let  miNumero =  5;
@@ -47,8 +55,8 @@ console.log(miApellido);
 let miVerdad = true;
 let miMentira = false;
 
-console.log(miVerdad)
-console.log(miMentira)
+console.log(miVerdad);
+console.log(miMentira);
 
 /* Underfined */
 
@@ -56,14 +64,47 @@ let x; /* Declarar una variable sin inicializada */
 
 console.log(x); // underfined
 
-x = prompt("¿Cual es tu numero favorito?")
+/* x = prompt("¿Cual es tu numero favorito?") */
 
-console.log)(x); // Valor que el usuario introduzca. 
+console.log(x); // Valor que el usuario introduzca. 
 
 /* Array */
 
 let miFrutasFavoritas = ["Manzana", "Uvas", "Limones"];
-let misNumerosSuerte ["3", 6, 9, 29]
+let misNumerosSuerte =  ["3", 6, 9, 29];
+let otroArray = ["tacos", 57, 89];
 
-console.log(miFrutasFavoritas)
-console.log(misNumerosSuerte)
+console.log(miFrutasFavoritas);
+console.log(misNumerosSuerte);
+console.log(otroArray);
+
+/* Objeto */
+
+let persona = {
+  // clave : valor,
+  nombre : "Gadiel",
+  edad : 19,
+  colorCabello : "Negro",
+  colorOjos : "Cafe oscuros",
+  altura : 1.82,
+  peso : undefined,
+  vivo : true,
+  novia : false,
+}
+
+console.log(persona);
+//Accedemos al valor de una clave dentro  de un objeto
+// Con la notacion de punto
+console.log(persona.nombre)
+console.log(persona.altura)
+console.log(persona.novia)
+
+/* Function */
+
+let mensajito = function () {
+  // Pasamos el codigo que ejecuta la funcion
+  console("Este es un mensaje lanzado por una función")
+}
+
+// Invocamos la funcion
+mensajito();
